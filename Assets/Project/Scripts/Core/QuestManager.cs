58e7e6d87000201f19281efa8ae13fcf92611eb3
IzmaylovNikita <nikitaizmaylov1402@gmail.com>
2025-08-02 01:07:26 +0300

    ))

---
+++ Assets/Project/Scripts/Core/QuestManager.cs
@@ -0,0 +1,28 @@
+﻿using Naninovel;
+using Project.Scripts.Core;
+using UnityEngine;
+
+[InitializeAtRuntime]
+public class QuestManager : IEngineService
+{
+    private QuestUI questUI;
+    
+    public string CurrentQuest { get; private set; }
+
+    public UniTask InitializeServiceAsync()
+    {
+        questUI = Engine.GetService<IUIManager>().GetUI<QuestUI>();
+        return UniTask.CompletedTask;
+    }
+
+    public void UpdateQuest(string text)
+    {
+        CurrentQuest = text;
+        questUI.UpdateQuest(text);
+    }
+
+    public void CompleteQuest() => questUI.Hide();
+
+    public void ResetService() => CurrentQuest = null;
+    public void DestroyService() {}
+}
