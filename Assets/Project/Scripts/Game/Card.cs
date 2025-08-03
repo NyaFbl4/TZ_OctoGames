290db409debb2bff3707b8b4f297c2851d493322
IzmaylovNikita <nikitaizmaylov1402@gmail.com>
2025-08-04 01:46:24 +0300

    Карты

---
+++ Assets/Project/Scripts/Game/Card.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Project.Scripts.Game
+{
+    public class Card : MonoBehaviour
+    {
+        [SerializeField] private Image _image;
+        [SerializeField] private Sprite _frontSprite;
+
+        public Action<Card> OnCardClicked;
+
+        public void Clicked()
+        {
+            OnCardClicked?.Invoke(this);
+            Debug.Log(gameObject.name);
+        }
+    }
+}
---
+++ Assets/Project/Scripts/Game/GameUI.cs
@@ -0,0 +1,12 @@
+﻿using Naninovel.UI;
+
+namespace Project.Scripts.Game
+{
+    public class GameUI : CustomUI
+    {
+        protected override void Awake()
+        {
+            base.Awake();
+        }
+    }
+}
